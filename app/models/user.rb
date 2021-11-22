c034ce3057d7b82d21794e6629ebce3fd53ec0f9
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-11-22 16:01:16 -0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :confirmable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  has_many :article, dependent: :destroy
+
+  # Caso UID não aparecer para a confirmação de email, descomenar estas linhas e aparagar os parenteses
+  # before_validation :set_uid
+  # private
+  # def set_uid
+  #   self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  # end )
+end
